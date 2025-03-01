e48e52b5e1ea12cabf79d314488cf3ee31422fcc
belyash <belyakov06mat@mail.ru>
2025-03-01 17:15:09 +0300

    Adding HW15

--- bioinf.py
+++ bioinf.py
@@ -73,53 +73,6 @@ def filter_fastq(input_fastq: str, output_fastq: str, gc_bounds: Union[int, tupl
             file.close()
 
 
-# def run_dna_rna_tools(*args: str) -> Union[str, List[str]]:
-#     """
-#     Function run_dna_rna_tools
-#     Application - Several string values are supplied to the function input, the last of which is the type of operation applied to the sequences.
-#     This function returns the result of the applied operation to the sequence, if it is possible to perform it, otherwise it is printed:
-#         1. error_string = 'Invalid input: string must be either DNA or RNA', if the sequence is not DNA or RNA
-#         2. error_string_rna = "The string must be DNA. Given an RNA string", if the operation transcribe_dna_complement is attempted
-#         to RNA sequence.
-#
-#     Attributes
-#     ----------
-#     *args : str или List[str]
-#         args - a tuple containing all sequences of input data and the operation applied to them.
-#     """
-#     operator: str = args[-1]
-#     seqs: Union[str, List[str]] = list(args[:-1])
-#     output: List[str] = []
-#     seqs: List[str] = list(seqs)
-#     correct_seqs: List[str] = []
-#     type_: List[int] = []
-#     for ind in range(len(seqs)):
-#         letters: Set[str] = set(seqs[ind])
-#         checker = all(ch in "AUTGCautgc" for ch in letters) and not (
-#                 any(ch in 'Tt' for ch in letters) and any(ch in 'Uu' for ch in letters))
-#         if not checker:
-#             correct_seqs.append(utils.error_string)
-#             continue
-#         correct_seqs.append(seqs[ind])
-#         if 'U' in letters or 'u' in letters:
-#             type_.append(0)
-#             continue
-#         type_.append(1)
-#     for seq in range(len(correct_seqs)):
-#         if correct_seqs[seq] != utils.error_string:
-#             if operator == 'transcribe_dna_complement':
-#                 if not type_[seq]:
-#                     output.append(utils.error_string_rna)
-#                     continue
-#             if correct_seqs[seq] != utils.error_string:
-#                 output.append(operations[operator](correct_seqs[seq], type_[seq]))
-#         else:
-#             output.append(correct_seqs[seq])
-#
-#     if len(output) > 1:
-#         return output
-#     return output[0]
-
 
 class BiologicalSequence(ABC):
     def __init__(self, sequence: str, alphabet: str):
